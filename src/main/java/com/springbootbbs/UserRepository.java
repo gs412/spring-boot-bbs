6c5ed48f0642c45cb631a43a3d61f9c5ef79e9f7
gengshuai <gs412@126.com>
2018-12-22 22:38:47 +0800

    modified:   src/main/java/com/springbootbbs/UserRepository.java 	modified:   src/main/java/com/springbootbbs/UserService.java

--- src/main/java/com/springbootbbs/UserRepository.java
+++ src/main/java/com/springbootbbs/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	public User findByUsername(String username);
+
+	public Boolean existsByUsername(String username);
+
 }
--- src/main/java/com/springbootbbs/UserService.java
+++ src/main/java/com/springbootbbs/UserService.java
@@ -1,5 +1,6 @@
 package com.springbootbbs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,6 +17,9 @@ public class UserService {
 	// 用户的集合
 	private List<User> users = new ArrayList<>();
 
+	@Autowired
+	UserRepository userRepository;
+
 	public UserService() {
 		// 从数据库查出来的用户名，密码，这是是静态数据(密码是123456)
 		users.add(new User("admin", "038bdaf98f2037b31f1e75b5b4c9b26e"));
@@ -24,23 +28,18 @@ public class UserService {
 
 	// 判断是否用户名是否存在
 	public boolean selectUsername(String username) {
-		for (User user : users) {
-			if (user.getUsername().equals(username)) {
-				return true;
-			}
-		}
-
-		return false;
+		return userRepository.existsByUsername(username);
 	}
 
 	// 根据用户返回查询的密码
 	public String selectPassword(String username) {
-		for (User user : users) {
-			if (user.getUsername().equals(username)) {
-				return user.getPassword();
-			}
+		User user = userRepository.findByUsername(username);
+
+		if (user == null) {
+			return "";
+		} else {
+			return user.getPassword();
 		}
-		return "";
 	}
 
 }
