6b0c34f9c0a298dcd8adcebece6689973f479d4f
gengshuai <gs412@126.com>
2018-12-24 00:03:10 +0800

    modified:   pom.xml 	modified:   spring-boot-bbs.iml 	modified:   src/main/java/com/springbootbbs/UserController.java 	modified:   src/main/resources/templates/user/register.ftl

--- src/main/java/com/springbootbbs/UserController.java
+++ src/main/java/com/springbootbbs/UserController.java
@@ -1,13 +1,17 @@
 package com.springbootbbs;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/user")
@@ -27,7 +31,35 @@ public class UserController {
 
 	@RequestMapping("/register")
 	public String register(ModelMap m) {
-		m.addAttribute("name", "haha");
+		return "user/register";
+	}
+
+	@RequestMapping("/register_save")
+	public String register_save(String username, String password, String password_confirm, ModelMap m) {
+		username = StringUtils.trimToEmpty(username);
+		password = StringUtils.trimToEmpty(password);
+		password_confirm = StringUtils.trimToEmpty(password_confirm);
+
+		String msg = "";
+
+		if (username.length() < 3) {
+			msg = "用户名最少3个字符";
+		} else if (username.length() > 20) {
+			msg = "用户名最多20个字符";
+		} else if (!Pattern.matches("^[\\w_]+$", username)) {
+			msg = "用户名只可以是字母、数字、下划线";
+		} else if (Pattern.matches("^\\d.*", username)) {
+			msg = "用户名不可以数字开头";
+		} else if (password.length() < 6) {
+			msg = "密码最少6个字符";
+		} else if (!password.equals(password_confirm)) {
+			msg = "两次密码不一致";
+		} else {
+			Object hashedPassword = new SimpleHash("md5", password, username, 1024);
+		}
+
+		m.addAttribute("username", username);
+		m.addAttribute("msg", msg);
 
 		return "user/register";
 	}
--- src/main/resources/templates/user/register.ftl
+++ src/main/resources/templates/user/register.ftl
@@ -1,1 +1,7 @@
-good ${name}
+<h4>${msg!}</h4>
+<form action="/user/register_save" method="post">
+	用户名：<input type="text" name="username" value="${username!}"><br>
+	密码：<input type="password" name="password"><br>
+	确认密码：<input type="password" name="password_confirm"><br>
+	<input type="submit" value="注册">
+</form>
