b7592d65a3e774b3090309df5d733308b375974d
gengshuai <gs412@126.com>
2018-12-27 13:35:56 +0800

    modified:   src/main/resources/templates/topic/topic_show.ftl 	new file:   src/main/java/com/springbootbbs/libs/

---
+++ src/main/java/com/springbootbbs/libs/Utils.java
@@ -0,0 +1,27 @@
+package com.springbootbbs.libs;
+
+public class Utils {
+
+	public static String subTextString(String str,int len) {
+		return subTextString(str, len, "");
+	}
+
+	public static String subTextString(String str,int len, String suffix){
+		if(str.length()<len/2)return str;
+		int count = 0;
+		StringBuffer sb = new StringBuffer();
+		String[] ss = str.split("");
+		for(int i=1;i<ss.length;i++){
+			count+=ss[i].getBytes().length>1?2:1;
+			sb.append(ss[i]);
+			if(count>=len)break;
+		}
+		//不需要显示...的可以直接return sb.toString();
+		if (sb.toString().length()<str.length()) {
+			return sb.append(suffix).toString();
+		} else {
+			return str;
+		}
+	}
+
+}
--- src/main/resources/templates/topic/topic_show.ftl
+++ src/main/resources/templates/topic/topic_show.ftl
@@ -3,7 +3,7 @@
 <h2>${topic.getTitle()}</h2>
 
 <#list page.getContent() as post>
-    <div style="border:1px solid #666;">
+    <div style="border:1px solid #666;" class="clearfix">
 	    <div style="min-height:200px; border-right:1px solid #666; float:left; width:200px;">
 	        ${post.user.getUsername()}
 	    </div>
