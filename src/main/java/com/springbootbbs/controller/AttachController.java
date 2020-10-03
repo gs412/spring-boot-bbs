17c5b45e8c1153b876d1dde5ac355c75cf7a0662
gengshuai <gs412@126.com>
2020-10-04 00:56:33 +0800

    modified:   src/main/java/com/springbootbbs/controller/AttachController.java

--- src/main/java/com/springbootbbs/controller/AttachController.java
+++ src/main/java/com/springbootbbs/controller/AttachController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
