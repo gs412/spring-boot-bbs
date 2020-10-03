b6ae947a3e0b6081fdf9f6382ebf7dc77d39d514
gengshuai <gs412@126.com>
2020-10-03 21:30:30 +0800

    modified:   src/main/java/com/springbootbbs/controller/AttachController.java

--- src/main/java/com/springbootbbs/controller/AttachController.java
+++ src/main/java/com/springbootbbs/controller/AttachController.java
@@ -1,4 +1,25 @@
 package com.springbootbbs.controller;
 
+import com.springbootbbs.entiry.Attach;
+import com.springbootbbs.repository.AttachRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/attach")
 public class AttachController extends BaseController {
+
+    @Autowired
+    AttachRepository attachRepository;
+
+    @RequestMapping(value = "/show/{id}")
+    public String show(@PathVariable Long id) {
+        Attach attach = attachRepository.findById(id).get();
+
+
+        return "";
+    }
+
 }
