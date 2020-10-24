3f454f094c60ceba7e844f2a80fce551ce0f13a1
gengshuai <gs412@126.com>
2020-10-25 01:49:13 +0800

    modified:   src/main/java/com/springbootbbs/repository/TopicRepository.java

--- src/main/java/com/springbootbbs/repository/TopicRepository.java
+++ src/main/java/com/springbootbbs/repository/TopicRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface TopicRepository extends PagingAndSortingRepository<Topic, Long> {
