e7e53c3b28dc05a3131c4f863783cb6efca5b737
gengshuai <gs412@126.com>
2022-05-17 18:28:05 +0800

    add remember me 	modified:   pom.xml 	modified:   src/main/java/com/springbootbbs/config/ShiroConfigBean.java

--- src/main/java/com/springbootbbs/config/ShiroConfigBean.java
+++ src/main/java/com/springbootbbs/config/ShiroConfigBean.java
@@ -5,7 +5,9 @@ import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -89,6 +91,8 @@ public class ShiroConfigBean {
         // 注入缓存管理器;
 		securityManager.setCacheManager(ehCacheManager());
 
+		securityManager.setRememberMeManager(rememberMeManager());
+
 		return securityManager;
 	}
 
@@ -123,6 +127,25 @@ public class ShiroConfigBean {
 		EhCacheManager cacheManager = new EhCacheManager();
 		cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
 		return cacheManager;
+	}
+
+	@Bean
+	public SimpleCookie rememberMeCookie(){
+		//这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
+		SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+		//cookie生效时间30天,单位秒;
+		simpleCookie.setMaxAge(2592000);
+		return simpleCookie;
+	}
+
+	@Bean
+	public CookieRememberMeManager rememberMeManager(){
+		CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+		cookieRememberMeManager.setCookie(rememberMeCookie());
+		// cookieRememberMeManager.setCipherKey用来设置加密的Key,参数类型byte[],字节数组长度要求16
+		// cookieRememberMeManager.setCipherKey(Base64.decode("3AvVhmFLUs0KTA3Kprsdag=="));
+		cookieRememberMeManager.setCipherKey("springbootbbskey".getBytes());
+		return cookieRememberMeManager;
 	}
 
 }
