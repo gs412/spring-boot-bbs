cc4dbab2106ca056b5d2d3afb125a5f1471e14aa
gengshuai <gs412@126.com>
2022-05-17 23:57:03 +0800

    modified:   src/main/java/com/springbootbbs/Application.java

--- src/main/java/com/springbootbbs/Application.java
+++ src/main/java/com/springbootbbs/Application.java
@@ -20,7 +20,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-        System.out.println("SUCCESS ^_^\n");
+        System.out.println("启动成功\n");
 	}
 
 }
