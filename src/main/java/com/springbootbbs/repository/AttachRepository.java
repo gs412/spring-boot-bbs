6673b66ffe3465f4666e1cb0d26dcad66d85dd87
gengshuai <gs412@126.com>
2022-05-18 08:47:20 +0800

    modified:   src/main/java/com/springbootbbs/repository/AttachRepository.java

--- src/main/java/com/springbootbbs/repository/AttachRepository.java
+++ src/main/java/com/springbootbbs/repository/AttachRepository.java
@@ -2,7 +2,9 @@ package com.springbootbbs.repository;
 
 import com.springbootbbs.entiry.Attach;
 import com.springbootbbs.entiry.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,5 +17,12 @@ public interface AttachRepository extends PagingAndSortingRepository<Attach, Lon
     //Attach findTopByOwnerIdAndOwnerTypeAndUserId(Long ownerId, Attach.OwnerType ownerType, Long userId);
 
     List<Attach> findAllByOwnerIdAndOwnerTypeAndUser(Long ownerId, Attach.OwnerType ownerType, User user);
+
+    @Query("update bbs_attach set owner_id = :ownerId where owner_id = :oldOwnerId and owner_type = :ownerType and user_id = :#{#user.getId()}")
+    void updateOwnerIdByOwnerIdAndOwnerTypeAndUser(
+            @Param("ownerId") Long ownerId,
+            @Param("oldOwnerId") Long oldOwnerId,
+            @Param("ownerType") Attach.OwnerType ownerType,
+            @Param("user") User user);
 
 }
