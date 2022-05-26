eaa358419e2a102d0c3acd407f2437f3d3d573f6
gengshuai <gs412@126.com>
2022-05-26 13:36:04 +0800

    modified:   src/main/java/com/springbootbbs/libs/helpers/Local.java

--- src/main/java/com/springbootbbs/libs/helpers/Local.java
+++ src/main/java/com/springbootbbs/libs/helpers/Local.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Local implements TemplateMethodModelEx {
     @Override
-    public Object exec(List list) throws TemplateModelException {
+    public Object exec(List list) {
         String message = I18nUtil.getMessage(list.get(1).toString(), list.get(0).toString());
 
         return message;
