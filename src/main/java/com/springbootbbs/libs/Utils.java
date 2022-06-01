ae994160fd4187323409a0b115dd867198ffb189
gengshuai <gs412@126.com>
2022-06-01 15:05:34 +0800

    modified:   src/main/java/com/springbootbbs/libs/Utils.java

--- src/main/java/com/springbootbbs/libs/Utils.java
+++ src/main/java/com/springbootbbs/libs/Utils.java
@@ -5,8 +5,10 @@ import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.data.MutableDataSet;
+import org.springframework.util.ClassUtils;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Utils {
 
@@ -33,7 +35,7 @@ public class Utils {
 	}
 
     public static String getBasePath() {
-        return System.getProperty("user.dir");
+        return Objects.requireNonNull(Objects.requireNonNull(ClassUtils.getDefaultClassLoader()).getResource("")).getPath();
     }
 
     public static String makeQueryStr(HashMap<String, String> allRequestParams) {
