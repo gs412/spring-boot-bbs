9cf04279297c29b4b4ee20a49117e0dc30315622
gengshuai <gs412@126.com>
2022-09-13 20:48:54 +0800

    modified:   src/main/java/com/springbootbbs/controller/ProfileController.java

--- src/main/java/com/springbootbbs/controller/ProfileController.java
+++ src/main/java/com/springbootbbs/controller/ProfileController.java
@@ -11,10 +11,7 @@ import com.springbootbbs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
@@ -75,6 +72,15 @@ public class ProfileController extends BaseController {
 
         String json = new ObjectMapper().writeValueAsString(map);
         return json;
+    }
+
+    @GetMapping("/password")
+    public String password(ModelMap m) {
+        User user = getUser();
+
+        m.addAttribute("user", user);
+
+        return "profile/password";
     }
 
 }
