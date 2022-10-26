4c67eaa63a5e777cf4d2ecd00ddc2eb9638fe2d6
gengshuai <gs412@126.com>
2022-10-26 13:07:45 +0800

    modified:   src/main/java/com/springbootbbs/controller/AttachController.java   	new file:   src/main/resources/vendor/

--- src/main/java/com/springbootbbs/controller/AttachController.java
+++ src/main/java/com/springbootbbs/controller/AttachController.java
@@ -7,6 +7,7 @@ import com.springbootbbs.repository.UserRepository;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -51,8 +52,14 @@ public class AttachController extends BaseController {
         User user = userRepository.findById(uid).get();
         Attach attach = user.getUserFace(attachRepository);
 
-        File file = new File(attach.getAbsolutePath());
-        response.setContentType(attach.getContentType());
+        File file;
+        if (attach != null) {
+            file = new File(attach.getAbsolutePath());
+            response.setContentType(attach.getContentType());
+        } else {
+            file = ResourceUtils.getFile("classpath:vendor/images/userface.jpg");
+            response.setContentType("image/jpeg");
+        }
         InputStream in=new FileInputStream(file);
         IOUtils.copy(in, response.getOutputStream());
     }
