46e6a41985377a9f253cfec4dcf324791f4df3fe
gengshuai <gs412@126.com>
2022-10-27 02:59:11 +0800

    modified:   src/main/java/com/springbootbbs/controller/IndexController.java

--- src/main/java/com/springbootbbs/controller/IndexController.java
+++ src/main/java/com/springbootbbs/controller/IndexController.java
@@ -21,11 +21,17 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Random;
 
 
 @Controller
@@ -95,6 +101,53 @@ public class IndexController extends BaseController {
         response.addCookie(cookie);
 
         return "redirect:" + request.getHeader("referer");
+    }
+
+    @RequestMapping("/seccode/en")
+    @ResponseBody
+    public String seccodeEn(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        long timestamp = new Date().getTime();
+        Random rand = new Random();
+        int randInt = rand.nextInt(999999 - 100000 + 1) + 100000;
+        String fileNamePre = "seccode_en_" + timestamp + "_" + randInt + "_";
+
+        ArrayList<String> rgb = new ArrayList<>();
+        rgb.add(String.valueOf(rand.nextInt(210-10) + 10));
+        rgb.add(String.valueOf(rand.nextInt(210-10) + 10));
+        rgb.add(String.valueOf(rand.nextInt(210-10) + 10));
+
+        String[] chars = "ABCDEFGHJKLMNPQRSTUVWXY".split("");
+        StringBuilder text = new StringBuilder();
+        for (int i=0; i<4; i++) {
+            text.append(chars[rand.nextInt(chars.length)]);
+        }
+
+        session.setAttribute("seccode", text);
+
+        for (int i=0; i<4; i++) {
+            String chr = text.toString().split("")[i];
+            makeFontPngs(chr, i, fileNamePre, rgb);
+        }
+
+        return fileNamePre;
+    }
+
+    private void makeFontPngs(String chr, int i, String fileNamePre, ArrayList<String> rgb) {
+        String textColor = "rgba(" + String.join(", ", rgb) + ", 1)";
+        int rotate = 90;
+        String command = """
+                convert -size 40x40 -fill '#{text_color}' -background none \
+                -swirl #{%w[- +].sample+rand(20..30).to_s} \
+                -rotate -#{rotate} \
+                -wave #{%w[- +].sample+rand(2..4).to_s}x#{rand(120..150)} \
+                -rotate +#{rotate} \
+                -wave #{%w[- +].sample+rand(2..4).to_s}x#{rand(120..150)} \
+                -font '#{Rails.root}/vendor/seccode/fonts/huawencaiyun.ttf' \
+                -pointsize 40 -gravity Center label:#{char} \
+                /tmp/#{@file_name_pre}#{i}.png
+                """;
+
+        System.out.println(command);
     }
 
 }
