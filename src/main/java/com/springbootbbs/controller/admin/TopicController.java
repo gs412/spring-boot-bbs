26a541de658619361f09b696959f707d84bb45bb
gengshuai <gs412@126.com>
2022-11-05 01:06:52 +0800

    modified:   src/main/java/com/springbootbbs/controller/admin/TopicController.java

--- src/main/java/com/springbootbbs/controller/admin/TopicController.java
+++ src/main/java/com/springbootbbs/controller/admin/TopicController.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
